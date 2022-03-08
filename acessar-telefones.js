9f58a108e9a495d5a193494773b8e60a5d49b899
Victor Ramon <victoramon007@gmail.com>
2022-03-08 12:14:21 -0300

    Adição dos arquivos da aula 02

---
+++ acessar-telefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Victor",
+    idade: 27,
+    cpf: "25648975132",
+    email: "victor@email.com",
+    fones: ["5587912345678", "5547956848965"]
+}
+
+cliente.fones.forEach(telefone => console.log(telefone));
---
+++ compondo-objetos.js
@@ -0,0 +1,6 @@
+const cliente = {
+    nome: "Victor",
+    idade: 27,
+    cpf: "25648975132",
+    email: "victor@email.com" 
+}
