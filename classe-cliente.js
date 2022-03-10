0f28f0c26a4c356c4cd4332c7a8a2c96767893bb
Victor Ramon <victoramon007@gmail.com>
2022-03-10 11:23:09 -0300

    Arquivos aula 5

---
+++ classe-cliente.js
@@ -0,0 +1,22 @@
+class Cliente{
+    constructor(nome, email, cpf, saldo){
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.saldo = saldo
+    }
+
+    depositar(valor){
+        this.saldo += valor
+    }
+
+    exibirSaldo(){
+        console.log(this.saldo)
+    }
+}
+
+const andre = new Cliente("Andre", "andre@email.com", "78945412304", 100)
+
+console.log(andre);
+
+andre.exibirSaldo()
---
+++ classe-clientepoup.js
@@ -0,0 +1,36 @@
+class Cliente{
+    constructor(nome, email, cpf, saldo){
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.saldo = saldo
+    }
+
+    depositar(valor){
+        this.saldo += valor
+    }
+
+    exibirSaldo(){
+        console.log(this.saldo)
+    }
+}
+
+class ClientePoupanca extends Cliente {
+    constructor(nome, email, cpf, saldo, saldoPoupanca){
+        super(nome, email, cpf, saldo)
+        this.saldoPoupanca = saldoPoupanca
+    }
+
+    depositarPoupanca(valor){
+        this.saldoPoupanca += valor
+    }
+}
+
+const andre = new ClientePoupanca("André", "andre@email.com", "69332548454", 100, 200)
+
+console.log(andre)
+
+andre.depositar(30)
+andre.depositarPoupanca(50)
+
+console.log(andre)
